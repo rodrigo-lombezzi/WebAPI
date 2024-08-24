d973964961d7d32710ca1d6e68782616bceac7a9
rodrigo-lombezzi <rodrigoylombezzi@gmail.com>
2024-08-24 10:22:24 -0300

    :sparkles: feat: Construindo DTO

---
+++ ReserveiAPI/Contexts/AppDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReserveiAPI.Objects.Models.Entities;
+using ReserveiAPI.Contexts.Builders;
+
+namespace ReserveiAPI.Contexts
+{
+    public class AppDBContext : DbContext
+    {
+        // Mapeamento Relacional dos Objetos no Banco de Dados
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) { } 
+        // Conjunto: Usuário
+        public DbSet<UserModel> Users { get; set; }
+        //Fluent API
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Entidades de Usuário:
+            UserBuilder.Build(modelBuilder);
+        }
+    }
+}
---
+++ ReserveiAPI/Contexts/Builders/UserBuilder.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ReserveiAPI.Objects.Models.Entities;
+
+namespace ReserveiAPI.Contexts.Builders
+{
+    public class UserBuilder
+    {
+        public static void Build(ModelBuilder modelBuilder)
+        {
+            //Builder
+            modelBuilder.Entity<UserModel>().HasKey(u => u.Id);
+            modelBuilder.Entity<UserModel>().Property(u => u.ImageProfile);
+            modelBuilder.Entity<UserModel>().Property(u => u.NameUser).HasMaxLength(100).IsRequired();
+            modelBuilder.Entity<UserModel>().Property(u => u.EmailUser).HasMaxLength(200).IsRequired();
+            modelBuilder.Entity<UserModel>().Property(u => u.PasswordUser).HasMaxLength(256).IsRequired();
+            modelBuilder.Entity<UserModel>().Property(u => u.PhoneUser).HasMaxLength(15).IsRequired();
+
+            //Inserções
+            modelBuilder.Entity<UserModel>().HasData(
+                new UserModel
+                {
+                    Id = 1,
+                    NameUser = "Master",
+                    EmailUser = "master@development.com",
+                    PasswordUser = "313d99eeb6420a37519a343c26296ad00b7c25fb53bf236550d700969802dd5c",
+                    PhoneUser = "",
+                    ImageProfile = ""
+                }
+            );
+        }
+    }
+}
