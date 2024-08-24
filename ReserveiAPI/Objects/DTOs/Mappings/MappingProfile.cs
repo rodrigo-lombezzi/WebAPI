71724e3c4b645d05de37bb579d55227386474202
rodrigo-lombezzi <rodrigoylombezzi@gmail.com>
2024-08-24 10:54:32 -0300

    :sparkles: feat: MappingProfile

---
+++ ReserveiAPI/Objects/DTOs/Mappings/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using ReserveiAPI.Objects.DTOs.Entities;
+using ReserveiAPI.Objects.Models.Entities;
+
+namespace ReserveiAPI.Objects.DTOs.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Entidades de Usuário:
+            CreateMap<UserDTO, UserModel>().ReverseMap();
+        }
+    }
+   
+}
